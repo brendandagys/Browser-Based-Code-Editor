ce5f830800461ed9cadb4693e51529e1aecf97f7
Brendan Dagys <brendandagys@gmail.com>
2021-06-05 11:06:23 -0400

    Finished bundling logic and safe code execution

--- src/index.tsx
+++ src/index.tsx
@@ -6,13 +6,13 @@ import { fetchPlugin } from './plugins/fetch-plugin'
 
 const App = () => {
   const ref = useRef<any>()
+  const iframe = useRef<any>()
   const [input, setInput] = useState('')
-  const [code, setCode] = useState('')
 
   const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
-      wasmURL: '/esbuild.wasm',
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
     })
   }
 
@@ -30,6 +30,8 @@ const App = () => {
     //   target: 'es2015',
     // })
 
+    iframe.current.srcdoc = html
+
     const result = await ref.current.build({
       entryPoints: ['index.js'],
       bundle: true,
@@ -43,8 +45,35 @@ const App = () => {
 
     // console.log(result)
 
-    setCode(result.outputFiles[0].text)
+    // setCode(result.outputFiles[0].text)
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*')
+
+    // try {
+    //   eval(result.outputFiles[0].text)
+    // } catch (e) {
+    //   alert(e)
+    // }
   }
+
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (e) => {
+            try {
+              eval(e.data);
+            } catch (e) {
+              const root = document.querySelector('#root');
+              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + e + '</div>';
+              console.error(e);
+            }
+          }, false);
+        </script>
+      </body>
+    </html>
+  `
 
   return (
     <div>
@@ -55,7 +84,12 @@ const App = () => {
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <pre>{code}</pre>
+      <iframe
+        ref={iframe}
+        sandbox='allow-scripts'
+        srcDoc={html}
+        title='preview'
+      />
     </div>
   )
 }
--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -10,14 +10,14 @@ export const fetchPlugin = (inputCode: string) => {
   return {
     name: 'fetch-plugin',
     setup(build: esbuild.PluginBuild) {
-      build.onLoad({ filter: /.*/ }, async (args: any) => {
-        if (args.path === 'index.js') {
-          return {
-            loader: 'jsx',
-            contents: inputCode,
-          }
+      build.onLoad({ filter: /(^index\.js$)/ }, () => {
+        return {
+          loader: 'jsx',
+          contents: inputCode,
         }
+      })
 
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
         // Check to see if we've already fetched this file and if it's in cache
         const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
           args.path
@@ -25,28 +25,40 @@ export const fetchPlugin = (inputCode: string) => {
 
         // If so, return it immediately
         if (cachedResult) return cachedResult
+      })
 
+      build.onLoad({ filter: /.css$/ }, async (args: any) => {
         const { data, request } = await axios.get(args.path)
-
-        const fileType = args.path.match(/.css$/) ? 'css' : 'jsx'
 
         const escaped = data
           .replace(/\n/g, '')
           .replace(/"/g, '\\"')
           .replace(/'/g, "\\'")
 
-        const contents =
-          fileType === 'css'
-            ? `
-          const style = document.createElement('style');
-          style.innerText = '${escaped}';
-          document.head.appendChild(style);
-          `
-            : data
+        const contents = `
+  const style = document.createElement('style');
+  style.innerText = '${escaped}';
+  document.head.appendChild(style);
+  `
 
         const result: esbuild.OnLoadResult = {
           loader: 'jsx',
           contents,
+          resolveDir: new URL('./', request.responseURL).pathname,
+        }
+
+        // Store response in cache
+        await fileCache.setItem(args.path, result)
+
+        return result
+      })
+
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        const { data, request } = await axios.get(args.path)
+
+        const result: esbuild.OnLoadResult = {
+          loader: 'jsx',
+          contents: data,
           resolveDir: new URL('./', request.responseURL).pathname,
         }
 
