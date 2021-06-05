d44bf993acc3bcd325d9b7f26eea15f3d91c1b93
Brendan Dagys <brendandagys@gmail.com>
2021-06-04 20:26:04 -0400

    Added CSS support

---
+++ src/plugins/fetch-plugin.ts
@@ -0,0 +1,60 @@
+import * as esbuild from 'esbuild-wasm'
+import axios from 'axios'
+import localForage from 'localforage'
+
+const fileCache = localForage.createInstance({
+  name: 'filecache',
+})
+
+export const fetchPlugin = (inputCode: string) => {
+  return {
+    name: 'fetch-plugin',
+    setup(build: esbuild.PluginBuild) {
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        if (args.path === 'index.js') {
+          return {
+            loader: 'jsx',
+            contents: inputCode,
+          }
+        }
+
+        // Check to see if we've already fetched this file and if it's in cache
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          args.path
+        )
+
+        // If so, return it immediately
+        if (cachedResult) return cachedResult
+
+        const { data, request } = await axios.get(args.path)
+
+        const fileType = args.path.match(/.css$/) ? 'css' : 'jsx'
+
+        const escaped = data
+          .replace(/\n/g, '')
+          .replace(/"/g, '\\"')
+          .replace(/'/g, "\\'")
+
+        const contents =
+          fileType === 'css'
+            ? `
+          const style = document.createElement('style');
+          style.innerText = '${escaped}';
+          document.head.appendChild(style);
+          `
+            : data
+
+        const result: esbuild.OnLoadResult = {
+          loader: 'jsx',
+          contents,
+          resolveDir: new URL('./', request.responseURL).pathname,
+        }
+
+        // Store response in cache
+        await fileCache.setItem(args.path, result)
+
+        return result
+      })
+    },
+  }
+}
