52561a42c835913614062d079fccf02c34d3722c
Brendan Dagys <brendandagys@gmail.com>
2021-06-08 19:56:57 -0400

    Finished Bundle fixes in redux

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,13 +1,16 @@
+import { Dispatch } from 'redux'
 import { ActionType } from '../action-types'
 import {
-  // Action,
   UpdateCellAction,
   DeleteCellAction,
   MoveCellAction,
   InsertCellAfterAction,
+  BundleStartAction,
+  BundleCompleteAction,
 } from '../actions'
 import { CellTypes } from '../cell'
 import { Direction } from '../actions'
+import bundle from '../../bundler'
 
 export const updateCell = (id: string, content: string): UpdateCellAction => {
   return {
@@ -46,5 +49,31 @@ export const insertCellAfter = (
       id,
       type: cellType,
     },
+  }
+}
+
+export const createBundle = (cellId: string, input: string) => {
+  return async (
+    dispatch: Dispatch<BundleStartAction | BundleCompleteAction>
+  ) => {
+    dispatch({
+      type: ActionType.BUNDLE_START,
+      payload: {
+        cellId,
+      },
+    })
+
+    const result = await bundle(input)
+
+    dispatch({
+      type: ActionType.BUNDLE_COMPLETE,
+      payload: {
+        cellId,
+        bundle: {
+          code: result.code,
+          error: result.error,
+        },
+      },
+    })
   }
 }
